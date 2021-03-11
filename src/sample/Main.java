ca625f0174d6a83aad8328efc24d64433e4915eb
timofeya123 <timofeya123@inbox.ru>
2021-03-11 23:53:08 +0600

    PasswordField

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -27,50 +27,91 @@ public class Main extends Application {
         root.setHgap(5);
         root.setVgap(5);
 
-        TextField name = new TextField("Ваше имя");
-        GridPane.setConstraints(name, 0,0);
+        //------------------------------------------------------- TextField ----------------------------------------------
+        // -------------------------------------- добавляем поля для ввода текста
+        TextField name = new TextField("Ваше имя");  // уже с тестом внутри
+        GridPane.setConstraints(name, 0,0); // место на панеле
         root.getChildren().add(name);
 
-        TextField lastName = new TextField();
+        TextField lastName = new TextField(); // обычный
         GridPane.setConstraints(lastName, 0,1);
         root.getChildren().add(lastName);
 
         TextField comment = new TextField();
-        comment.setPromptText("написать сюда");
+        comment.setPromptText("написать сюда коммент"); // подсказка для ввода
         GridPane.setConstraints(comment, 0,2);
         root.getChildren().add(comment);
 
-        Button button1 = new Button("click");
+        //  кнопки
+        Button button1 = new Button("обработать");
         GridPane.setConstraints(button1, 1,0);
         root.getChildren().add(button1);
 
-        Button button2 = new Button("clear");
+        Button button2 = new Button("очистить");
         GridPane.setConstraints(button2, 1,1);
         root.getChildren().add(button2);
 
+        // метка (лэйбл)
         Label label = new Label();
         GridPane.setConstraints(label, 3,0);
         root.getChildren().add(label);
 
-        button1.setOnAction(event -> {
+        button1.setOnAction(event -> { // действие на кнопку
             if (!comment.getText().isEmpty()){
-                label.setText(name.getText()+" "+lastName.getText()+" "+comment.getText());
+                label.setText(name.getText()+" "+lastName.getText()+" "+comment.getText()); // вытащить текст из TextField
             }else {
                 label.setText("Вы не написали коммент");
             }
         });
 
-        button2.setOnAction(event -> {
+        button2.setOnAction(event -> { // действие на кнопку (очистить)
             name.clear();
             lastName.clear();
             comment.clear();
             label.setText(null);
         });
 
+        //-------------------------------------------------------PasswordField  ----------------------------------------------
+        // -------------------------------------- добавляем поля для ввода пароля
+        PasswordField passwordField = new PasswordField();
+        passwordField.setPromptText("Ввидите пароль");
+        GridPane.setConstraints(passwordField, 0,3);
+        root.getChildren().add(passwordField);
+
+        //  кнопка
+        Button button3 = new Button("Проверить");
+        GridPane.setConstraints(button3, 1,3);
+        root.getChildren().add(button3);
+
+        // метка (лэйбл)
+        Label label2 = new Label("результат");
+        GridPane.setConstraints(label2, 2,3);
+        root.getChildren().add(label2);
+
+        button3.setOnAction(event -> { // действие на кнопку
+            PasswordFieldClick(passwordField, label2); // отсылаем ссылки на объекты в статическую функцию
+        });
+
+        passwordField.setOnAction(event -> { // действия внутри PasswordField, при нажатии на Enter
+            PasswordFieldClick(passwordField, label2); // отсылаем ссылки на объекты в статическую функцию
+        });
+
         // добавление на сцены | на форму
         Scene scene = new Scene(root, 500, 400);
         primaryStage.setScene(scene);  // размер формы и сцена
         primaryStage.show(); // отобразить
+    }
+
+    // статическая функция для PasswordField
+    static public void PasswordFieldClick(PasswordField passwordField, Label label){
+        if(!passwordField.getText().equals("123")){ // сверка
+            label.setText("Провал");
+            label.setTextFill(Color.RED);
+        }else{
+            label.setText("Успех"); // текст
+            label.setTextFill(Color.GREEN); // цвет
+        }
+        passwordField.clear(); // очистка
     }
 
     public static void main(String[] args) {
